33880d3d738168667fe44d88c0612bc496ce623f
Sérgio da Silva Albuquerque <sergio.albuquerquefilho@upe.br>
2025-12-09 22:08:59 -0300

    Update Morcego.java

--- inimigos/Morcego.java
+++ inimigos/Morcego.java
@@ -1,12 +1,11 @@
 package inimigos;
 
+import gerenciadores.GerenciadorInimigos;
 import static ajuda.Constantes.Inimigos.MORCEGO;
 
 public class Morcego extends Inimigo {
 
-	public Morcego(float x, float y, int ID) {
-		super(x, y, ID, MORCEGO);
-		
+	public Morcego(float x, float y, int ID, GerenciadorInimigos gerenciadorInimigos) {
+		super(x, y, ID, MORCEGO, gerenciadorInimigos);
 	}
-
 }
