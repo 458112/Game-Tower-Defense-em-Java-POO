112b3abe380c1e5f45cdebda590f8e58e60f5efd
Sérgio da Silva Albuquerque <sergio.albuquerquefilho@upe.br>
2025-12-09 22:08:39 -0300

    Modify Lobo constructor to include GerenciadorInimigos

--- inimigos/Lobo.java
+++ inimigos/Lobo.java
@@ -1,11 +1,11 @@
 package inimigos;
 
+import gerenciadores.GerenciadorInimigos;
 import static ajuda.Constantes.Inimigos.LOBO;
 
 public class Lobo extends Inimigo {
 
-	public Lobo(float x, float y, int ID) {
-		super(x, y, ID, LOBO);
+	public Lobo(float x, float y, int ID, GerenciadorInimigos gerenciadorInimigos) {
+		super(x, y, ID, LOBO, gerenciadorInimigos);
 	}
-
 }
