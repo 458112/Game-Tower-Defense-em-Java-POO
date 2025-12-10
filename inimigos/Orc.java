6e710062da558679a8ea635b00d366051506b10e
Sérgio da Silva Albuquerque <sergio.albuquerquefilho@upe.br>
2025-12-09 22:09:17 -0300

    Modify Orc constructor to include GerenciadorInimigos

--- inimigos/Orc.java
+++ inimigos/Orc.java
@@ -1,11 +1,11 @@
 package inimigos;
 
+import gerenciadores.GerenciadorInimigos;
 import static ajuda.Constantes.Inimigos.ORC;
 
 public class Orc extends Inimigo {
 
-	public Orc(float x, float y, int ID){
-		super(x, y, ID, ORC);
+	public Orc(float x, float y, int ID, GerenciadorInimigos gerenciadorInimigos) {
+		super(x, y, ID, ORC, gerenciadorInimigos);
 	}
-
 }
