5871a5ded81bcd8ff03afd571c6d7f901fbadd8d
Sérgio da Silva Albuquerque <sergio.albuquerquefilho@upe.br>
2025-12-09 22:07:35 -0300

    Modify Cavaleiro constructor to include manager

--- inimigos/Cavaleiro.java
+++ inimigos/Cavaleiro.java
@@ -1,12 +1,11 @@
 package inimigos;
 
+import gerenciadores.GerenciadorInimigos;
 import static ajuda.Constantes.Inimigos.CAVALEIRO;
 
 public class Cavaleiro extends Inimigo {
 
-    public Cavaleiro(float x, float y, int ID) {
-        super(x, y, ID, CAVALEIRO);
-        
-    }
-
+	public Cavaleiro(float x, float y, int ID, GerenciadorInimigos gerenciadorInimigos) {
+		super(x, y, ID, CAVALEIRO, gerenciadorInimigos);
+	}
 }
