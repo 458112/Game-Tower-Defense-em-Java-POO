f7dd3224a0f0e0f3ee29c7c289367f41874d60dc
Sérgio da Silva Albuquerque <sergio.albuquerquefilho@upe.br>
2025-12-09 22:10:40 -0300

    Refactor GerenciadorTorres for consistency and clarity

--- gerenciadores/GerenciadorTorres.java
+++ gerenciadores/GerenciadorTorres.java
@@ -12,68 +12,85 @@ import objetos.Torre;
 
 public class GerenciadorTorres {
 
-    private Jogando jogando;
-    private BufferedImage[] imgsTorre;
-    private ArrayList<Torre> torres = new ArrayList<>();
-    private int quantidadeTorres = 0;
+	private Jogando jogando;
+	private BufferedImage[] imgsTorre;
+	private ArrayList<Torre> torres = new ArrayList<>();
+	private int quantidadeTorres = 0;
 
-    public GerenciadorTorres(Jogando jogando) {
-        this.jogando = jogando;
-        carregarImgsTorre();
-    }
+	public GerenciadorTorres(Jogando jogando) {
+		this.jogando = jogando;
+		carregarImgsTorre();
+	}
 
-    private void carregarImgsTorre() {
-        BufferedImage atlas = CarregarSalvar.getAtlasSprites();
-        imgsTorre = new BufferedImage[3];
-        for (int i = 0; i < 3; i++)
-            imgsTorre[i] = atlas.getSubimage((4 + i) * 32, 32, 32, 32);
-    }
+	private void carregarImgsTorre() {
+		BufferedImage atlas = CarregarSalvar.getAtlasSprites();
+		imgsTorre = new BufferedImage[3];
+		for (int i = 0; i < 3; i++)
+			imgsTorre[i] = atlas.getSubimage((4 + i) * 32, 32, 32, 32);
+	}
 
-    public void addTorre(Torre torreSelecionada, int xPos, int yPos) {
-        torres.add(new Torre(xPos, yPos, quantidadeTorres++, torreSelecionada.getTipoTorre()));
-    }
+	public void addTorre(Torre torreSelecionada, int xPos, int yPos) {
+		torres.add(new Torre(xPos, yPos, quantidadeTorres++, torreSelecionada.getTipoTorre()));
+	}
+	
+	public void removerTorre(Torre torreExibida) {
+		for (int i = 0; i < torres.size(); i++)
+			if (torres.get(i).getId() == torreExibida.getId())
+				torres.remove(i);
+	}
+	
+	public void atualizarTorre(Torre torreExibida) {
+		for (Torre t : torres)
+			if (t.getId() == torreExibida.getId())
+				t.atualizarTorre();
+	}
 
-    public void atualizar() {
-        for (Torre t : torres) {
-            t.atualizar();
-            atacarInimigoSePerto(t);
-        }
-    }
+	public void atualizar() {
+		for (Torre t : torres) {
+			t.atualizar();
+			atacarInimigoSePerto(t);
+		}
+	}
 
-    private void atacarInimigoSePerto(Torre t) {
-        for (Inimigo e : jogando.getGerenciadorInimigos().getInimigos()) {
-            if (e.estaVivo())
-                if (estaInimigoNoAlcance(t, e)) {
-                    if (t.isCooldownOver()) {
-                        jogando.disparar(t, e);
-                        t.resetCooldown();
-                    }
-                }
-                else {
-                }
-        }
-    }
+	private void atacarInimigoSePerto(Torre t) {
+		for (Inimigo e : jogando.getGerenciadorInimigos().getInimigos()) {
+			if (e.estaVivo())
+				if (estaInimigoNoAlcance(t, e)) {
+					if (t.isCooldownOver()) {
+						jogando.disparar(t, e);
+						t.resetCooldown();
+					}
+				} else {
+					
+				}
+		}
+	}
 
-    private boolean estaInimigoNoAlcance(Torre t, Inimigo e) {
-        int alcance = Utilitarios.GetDistanciaHipotenusa(t.getX(), t.getY(), e.getX(), e.getY());
-        return alcance < t.getAlcance();
-    }
+	private boolean estaInimigoNoAlcance(Torre t, Inimigo e) {
+		int alcance = Utilitarios.GetDistanciaHipotenusa(t.getX(), t.getY(), e.getX(), e.getY());
+		return alcance < t.getAlcance();
+	}
 
-    public void desenhar(Graphics g) {
-        for (Torre t : torres)
-            g.drawImage(imgsTorre[t.getTipoTorre()], t.getX(), t.getY(), null);
-    }
+	public void desenhar(Graphics g) {
+		for (Torre t : torres)
+			g.drawImage(imgsTorre[t.getTipoTorre()], t.getX(), t.getY(), null);
+	}
 
-    public Torre getTorreEm(int x, int y) {
-        for (Torre t : torres)
-            if (t.getX() == x)
-                if (t.getY() == y)
-                    return t;
-        return null;
-    }
+	public Torre getTorreEm(int x, int y) {
+		for (Torre t : torres)
+			if (t.getX() == x)
+				if (t.getY() == y)
+					return t;
+		return null;
+	}
 
-    public BufferedImage[] getImgsTorre() {
-        return imgsTorre;
-    }
+	public BufferedImage[] getImgsTorre() {
+		return imgsTorre;
+	}
+	
+	public void resetar() {
+		torres.clear();
+		quantidadeTorres = 0;
+	}
 
 }
